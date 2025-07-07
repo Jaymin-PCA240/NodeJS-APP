c99dbf781acbe16b8302accc229a8a0132df8bde
Jaymin-PCA240 <151931444+Jaymin-PCA240@users.noreply.github.com>
2025-07-07 15:51:35 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (_req, res) => {
-  res.send('Hello from TypeScript + Express!');
+  res.send('Hello from TypeScript + Express from Github!');
 });
 
 app.listen(port, () => {
