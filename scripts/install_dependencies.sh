2a9073e0a8e5ebd0260e167010de77a42eaa136f
Jaymin Makwana <jaymin.makwana@tatvasoft.com>
2025-07-08 17:32:28 +0530

    fixed

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,10 +4,12 @@
 curl -sL https://rpm.nodesource.com/setup_18.x | sudo -E bash -
 sudo yum install -y nodejs
 
-# Navigate to app directory
+#!/bin/bash
 cd /home/ec2-user/myapp
-sudo chown -R ec2-user:ec2-user
 
-# Install production dependencies
-npm install --omit=dev
+# Correct permission fix
+sudo chown -R ec2-user:ec2-user /home/ec2-user/myapp
+
+# Install dependencies as the correct user
+sudo -u ec2-user npm install --omit=dev
 
