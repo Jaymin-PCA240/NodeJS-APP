222d481ce237d158384ebaa20409dd3832f928d8
Jaymin Makwana <jaymin.makwana@tatvasoft.com>
2025-07-09 18:23:54 +0530

    one at time mode

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (_req, res) => {
-  res.send('Hello from TypeScript + Express from Github. change in index file prod updated changes!');
+  res.send('Hello from TypeScript + Express from Github. change in index file prod updated changes One At a Time!');
 });
 
 app.listen(port, () => {
