1d18fe735a707add742d7903c9e12cf918bf20c2
Jaymin Makwana <jaymin.makwana@tatvasoft.com>
2025-07-09 11:35:48 +0530

    fixed

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-
-# Install Node.js and npm (Amazon Linux 2)
-curl -sL https://rpm.nodesource.com/setup_18.x | sudo -E bash -
-sudo yum install -y nodejs
-
-#!/bin/bash
 cd /home/ec2-user/myapp
 
 # Correct permission fix
